6f644638f213ab7ea49ae09af92076cb2dd5135a
gkushang <g.kushang@gmail.com>
2015-05-19 13:54:07 -0400

    move configuration

--- config/configuration.js
+++ config/configuration.js
@@ -1,7 +1,7 @@
 
 "use strict";
 
-var sauceConfig        = require('sauce.json');
+var sauceConfig        = require('./sauce.json');
 var _                  = require('underscore');
 
 function Configuration() {
