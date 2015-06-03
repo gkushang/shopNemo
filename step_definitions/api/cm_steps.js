b3f0efc4a27601b90f4916c6ccea9186b10f18ba
gkushang <g.kushang@gmail.com>
2015-06-03 18:05:11 -0400

    handling cucumber errors with cukes callbacks rather than in the utilities to avoid the False failures

--- step_definitions/api/cm_steps.js
+++ step_definitions/api/cm_steps.js
@@ -6,8 +6,15 @@ module.exports = function cm_steps() {
 
     this.Then(/^an Affiliate Offer is in Shop$/, function(callback) {
 
-        cm(this.nemo)
-            .create_affiliate_offer().done(callback);
+        var self = this;
 
+        cm(this.nemo)
+            .create_affiliate_offer()
+            .done(getAffiliateOffer, callback);
+
+        function getAffiliateOffer(affiliate_offer){
+            self.affiliate_offer = affiliate_offer;
+            callback();
+        }
     });
 };
